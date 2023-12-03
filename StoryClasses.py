1fd455918a6d2722c0907b12181bca2b79426b34
Jessamine McHugh <122129438+JessamineCodes@users.noreply.github.com>
2023-12-03 11:17:20 +0000

    Added comments, renamed file names to allow for multiple stories, changed Story class so now the story logic is a separate method that will change text returned depending on theme

---
+++ StoryClasses.py
@@ -0,0 +1,22 @@
+# Import method for creating space story text from SpaceStoryComponents.py
+from SpaceStoryComponents import space_story_text
+
+# Class for creating a story object - initialised with 3 inputs about the child and chosen theme
+class Story:
+    def __init__(self, child_name, child_pronouns, child_age, theme):
+        self.child_name = child_name
+        self.child_pronouns = child_pronouns
+        self.child_age = int(child_age)
+        self.theme = theme.lower()
+
+    # Method for generatin story text based on inputs
+    # Uses if statement to check which theme has been chosen and calls the relevant story text method
+    def generate_story(self):
+        if self.theme == "space":
+            return space_story_text(self.child_name, self.child_age)
+        else:
+            return "Theme not recognised"
+
+
+# Testing the class by creating a space story object and printing the story text
+print(Story("Jo", "she", "12", "space").generate_story())
