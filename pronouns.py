73e331e37c86d256fa58ef2ce427c541df1c5c83
Jessamine McHugh <122129438+JessamineCodes@users.noreply.github.com>
2023-12-11 16:19:07 +0000

    pronouns file added and pronoun logic integrated into story components and classes files

---
+++ pronouns.py
@@ -0,0 +1,9 @@
+# Dictionary of pronouns
+# assume user will input he, she, they or ze
+
+pronouns = {
+    "he": {"subject": "he", "object": "him", "possessive": "his", "reflexive": "himself"},
+    "she": {"subject": "she", "object": "her", "possessive": "her", "reflexive": "herself"},
+    "they": {"subject": "they", "object": "them", "possessive": "their", "reflexive": "themself"},
+    "ze": {"subject": "ze", "object": "zir", "possessive": "zir", "reflexive": "zirself"}
+}
