72db2ad00d55c2516342923334538b391f27f6e1
Jessamine McHugh <122129438+JessamineCodes@users.noreply.github.com>
2023-12-11 14:31:17 +0000

    created subclasses using abc, updated db_management to accept pokemon story, commented out line that was blocking my db creation

--- StoryClasses.py
+++ StoryClasses.py
@@ -4,40 +4,56 @@ from SpaceStoryComponents import space_story_text
 from DinosaurStoryComponents import dinosaur_story_text
 # Import method for creating pokemon story text from PokemonStoryComponents.py
 from PokemonStoryComponents import pokemon_story_text
-
+# Import abstract base class module
+import abc
 
 
 # Class for creating a story object - initialised with 3 inputs about the child and chosen theme
 class Story:
-    def __init__(self, child_name, child_pronouns, child_age, theme):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, child_name, child_pronouns, child_age):
         self.child_name = child_name
         self.child_pronouns = child_pronouns
         self.child_age = int(child_age)
-        self.theme = theme.lower()
 
-    # Method for generating story text based on inputs
-    # Uses if statement to check which theme has been chosen and calls the relevant story text method
+    @abc.abstractmethod
     def generate_story(self):
-        if self.theme == "space":
-            return space_story_text(self.child_name, self.child_age)
-        elif self.theme == "dinosaur":
-            return dinosaur_story_text(self.child_name, self.child_age)
-        elif self.theme == "pokemon":
-            return pokemon_story_text(self.child_name, self.child_age)
-        else:
-            return "Theme not recognised"
+        pass
+
+class SpaceStory(Story):
+    def __init__(self, child_name, child_pronouns, child_age):
+        super().__init__(child_name, child_pronouns, child_age)
+
+    def generate_story(self):
+        return space_story_text(self.child_name, self.child_age)
+
+class DinosaurStory(Story):
+    def __init__(self, child_name, child_pronouns, child_age):
+        super().__init__(child_name, child_pronouns, child_age)
+
+    def generate_story(self):
+        return dinosaur_story_text(self.child_name, self.child_age)
+
+class PokemonStory(Story):
+    def __init__(self, child_name, child_pronouns, child_age):
+        super().__init__(child_name, child_pronouns, child_age)
+
+    def generate_story(self):
+        return pokemon_story_text(self.child_name, self.child_age)
+
 
 # Testing the class by creating a space story object and printing the story text
-space_story_instance = Story("Jo", "she", "12", "space")
+space_story_instance = SpaceStory("Jo", "she", "12")
 space_story = space_story_instance.generate_story()
-print(space_story)
+# print(space_story)
 
 # Testing the class by creating a dino story object and printing the story text
-dinosaur_story_instance = Story("Rose", "she", "9", "dinosaur")
+dinosaur_story_instance = DinosaurStory("Rose", "she", "9")
 dinosaur_story = dinosaur_story_instance.generate_story()
-print(dinosaur_story)
+# print(dinosaur_story)
 
 # Testing the class by creating a pokemon story object and printing the story text
-pokemon_story_instance = Story("Max", "he", "8", "pokemon")
+pokemon_story_instance = PokemonStory("Max", "he", "8")
 pokemon_story = pokemon_story_instance.generate_story()
-print(pokemon_story)
+# print(pokemon_story)
