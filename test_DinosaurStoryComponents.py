48112b66d3fbea5c482129d0d3ec87335815564f
meganpark94 <143807636+meganpark94@users.noreply.github.com>
2023-12-15 22:20:15 +0000

    Unit test cases for:  - functions to create story components  - functions to retrieve API data

---
+++ test_DinosaurStoryComponents.py
@@ -0,0 +1,44 @@
+import unittest
+# import functions to be tested
+from DinosaurStoryComponents import (
+    paragraph_one,
+    paragraph_two,
+    paragraph_three,
+    dinosaur_story_text, )
+
+
+# test class for functions to create dinosaur story components
+
+class TestDinosaurStoryComponents(unittest.TestCase):
+    # test case for function to create paragraph one
+    def test_paragraph_one(self):
+        # call the function with example inputs
+        result = paragraph_one("Alex", 7, "he", "Velociraptor")
+        # check that the output contains an expected substring of the story
+        self.assertIn("Alex wanted to be a famous paleontologist", result)
+
+    # test case for function to create paragraph two
+    def test_paragraph_two(self):
+        # call the function with example inputs
+        result = paragraph_two("Emma", "she", "Triceratops")
+        # check that the output contains an expected substring of the story
+        self.assertIn("Emma found herself on a prehistoric beach", result)
+
+    # test case for function to create paragraph two
+    def test_paragraph_three(self):
+        # call the function with example inputs
+        result = paragraph_three("Sam", "he", "Stegosaurus", "with spiky plates on its back")
+        # check that the output contains an expected substring of the story
+        self.assertIn("Suddenly, there was a flash of light", result)
+
+    # test case for the function to create the full dinosaur story text
+    def test_dinosaur_story_text(self):
+        # call the function with example inputs
+        result = dinosaur_story_text("Taylor", 9, "they")
+        # check that the output contains two expected substrings of the story
+        self.assertIn("Taylor wanted to be a famous paleontologist", result)
+        self.assertIn("Suddenly, there was a flash of light", result)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_SpaceStoryComponents.py
@@ -0,0 +1,39 @@
+import unittest
+# import functions to be tested
+from SpaceStoryComponents import paragraph_one, paragraph_two, paragraph_three, space_story_text
+
+
+# test class for functions to create space story components
+class TestSpaceStoryFunctions(unittest.TestCase):
+    # test case for function to create paragraph one
+    def test_paragraph_one(self):
+        # call the function with example inputs
+        result = paragraph_one("Timmy", 8)
+        # Add your assertions here based on the expected output for given inputs
+        self.assertTrue("Timmy was the youngest ever astronaut" in result)
+
+    # test case for function to create paragraph two
+    def test_paragraph_two(self):
+        # call the function with example inputs
+        result = paragraph_two("Sally", "she")
+        # check that the output contains an expected substring of the story
+        self.assertIn("An unidentified lifeform has got stuck", result)
+        # test case for function to create paragraph three
+
+    def test_paragraph_three(self):
+        # call the function with example inputs
+        result = paragraph_three("Billy")
+        # check that the output contains an expected substring of the story
+        self.assertIn("From that day on they were best friends!", result)
+        # test case for function to create full space story
+
+    def test_space_story_text(self):
+        # call the function with example inputs
+        result = space_story_text("Tom", 10, "he")
+        # check that the output contains two expected substrings of the story
+        self.assertIn("Tom was the youngest ever astronaut", result)
+        self.assertIn("From that day on they were best friends!", result)
+
+
+if __name__ == '__main__':
+    unittest.main()
